6ffb568a7922ec65342f0ea587546d6c0cb8abd9
SudhanshuBaberwal <24bcs147@iiitdwd.ac.in>
2025-08-15 16:45:25 +0530

    minicontext project

---
+++ 08miniContext/src/Components/Login.jsx
@@ -0,0 +1,24 @@
+import React , {useState , useContext} from 'react'
+import UserContext from '../context/UsetContext'
+
+
+const Login = () => {
+    const [username , setUsername] = useState("")
+    const [password , setPassword ] = useState("")
+    const {setUser} = useContext(UserContext)
+
+    const handelSubmit = (e) => {
+        e.preventDefault()
+        setUser({username , password})
+    }
+  return (
+    <div>
+    <h2>Login</h2>
+    <input type="text" onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+    <input type="text" placeholder="Password" />
+    <button onClick={handelSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
